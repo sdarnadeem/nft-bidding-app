1927491421d41d2d57a078fbad580c67ef24a711
sdarnadeem <sdarnadeem@gmail.com>
2022-10-23 13:01:56 +0530

    add fonts

--- App.js
+++ App.js
@@ -16,7 +16,13 @@ const theme = {
 };
 
 export default function App() {
-  const [loaded] = useFonts({});
+  const [loaded] = useFonts({
+    InterBold: require("./assets/fonts/Inter-Bold.ttf"),
+    InterSemiBold: require("./assets/fonts/Inter-SemiBold.ttf"),
+    InterMedium: require("./assets/fonts/Inter-Medium.ttf"),
+    InterRegular: require("./assets/fonts/Inter-Regular.ttf"),
+    InterLight: require("./assets/fonts/Inter-Light.ttf"),
+  });
   return (
     <NavigationContainer>
       <Stack.Navigator
